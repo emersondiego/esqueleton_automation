4c2d0c09493bc7386effa9e4b4b332fa97f3d014
Emerson Pereira <emerson.pereira@sprinklr.com>
2016-11-17 16:26:46 -0200

    Include driver chrome

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source "https://rubygems.org"
 
-  gem "cucumber"
-  gem "capybara"
-  gem "selenium-webdriver"
-  gem "faker"
-  gem "site_prism"
-  gem "rspec"
+  gem 'cucumber'
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'faker'
+  gem 'site_prism'
+  gem 'rspec'
   gem 'rails'
   gem 'pry'
+  gem 'chromedriver-helper'
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,16 @@ require 'rails'
 Dir[File.dirname(__FILE__) + '/../helpers/*.rb'].each { |f| require f }
 include SiteHelpers
 
-Capybara.default_driver = :selenium
+##Executar com Firefox
+# Capybara.default_driver = :selenium
+# Capybara.default_max_wait_time = 10
+# Capybara.page.driver.browser.manage.window.maximize
+
+
+##Executar com Chrome
 Capybara.default_max_wait_time = 10
-Capybara.page.driver.browser.manage.window.maximize
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.default_driver = :selenium_chrome
+Capybara.javascript_driver = :chrome
