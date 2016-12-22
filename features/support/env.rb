3771f4e06ef94c6bab0306a1cd94bd076ff22607
Emerson Pereira <emerson.pereira@sprinklr.com>
2016-12-22 15:26:47 -0200

    update firefox

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,15 +9,14 @@ Dir[File.dirname(__FILE__) + '/../helpers/*.rb'].each { |f| require f }
 include SiteHelpers
 
 ##Executar com Firefox
-# Capybara.default_driver = :selenium
-# Capybara.default_max_wait_time = 10
-# Capybara.page.driver.browser.manage.window.maximize
-
+Capybara.default_driver = :selenium
+Capybara.default_max_wait_time = 10
+Capybara.page.driver.browser.manage.window.maximize
 
 ##Executar com Chrome
-Capybara.default_max_wait_time = 10
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-Capybara.default_driver = :selenium_chrome
-Capybara.javascript_driver = :chrome
+# Capybara.default_max_wait_time = 10
+# Capybara.register_driver :selenium_chrome do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
+# Capybara.default_driver = :selenium_chrome
+# Capybara.javascript_driver = :chrome
